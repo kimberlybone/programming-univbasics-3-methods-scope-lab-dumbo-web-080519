504dd173ccde7071132783cf5a3dbc9ba07f1b87
Kimberly Bone <k_bone@uncg.edu>
2019-06-12 16:20:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,15 +2,21 @@ def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
+mario
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+tosdstool
 
 def link 
+  puts "It's Dangerous To Go Alone! Take This."
+end
+link
   
 def any_phrase
   phrase = "Do A Barrel Roll!"
   puts phrase 
 end 
+any_phrase
