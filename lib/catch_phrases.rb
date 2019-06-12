a0a85e9ebf4d22b19b558773dc06f05592009d89
Kimberly Bone <k_bone@uncg.edu>
2019-06-12 16:20:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,7 +8,7 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
-tosdstool
+toadstool
 
 def link 
   puts "It's Dangerous To Go Alone! Take This."
